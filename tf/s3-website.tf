248f1d43814c3bcfea97f12456890754d577c8fe
Taymuraz Tolparov <tatolparov@avito.ru>
2023-10-28 09:36:11 +0300

    Add bucket policy

--- tf/s3-website.tf
+++ tf/s3-website.tf
@@ -7,3 +7,20 @@ resource "aws_s3_bucket_website_configuration" "website" {
 
   depends_on = [aws_s3_bucket.bucket-1, data.aws_s3_bucket.selected-bucket]
 }
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket-1.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.oai.iam_arn}"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket-1.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
